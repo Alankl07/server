26919e4bd6c6d60d9d1e8c712cd2ddee79bcb74c
alanNunes <alan.fsa.ans@gmail.com>
2020-09-29 00:05:48 -0300

    Ajuste

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AgendamentosModule } from './agendamentos/agendamentos.module';
-import { Conection } from '../src/serve/conection';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { NoitesModule } from './noites/noites.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AgendamentosModule, MongooseModule.forRoot(Conection), NoitesModule, UsersModule],
+  imports: [AgendamentosModule, MongooseModule.forRoot(process.env.MONGO_URL), NoitesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, ],
 })
